4bc536991b4bc27fa2500feeb42fc3b94d20e997
Ramdani Syaputra <rmdn.syaputra@gmail.com>
2025-07-11 18:38:30 +0700

    changes

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -25,8 +25,11 @@
             <div class="notification-slider">
                 <div class="d-flex h-100">
                     <img src="{{ asset('assets/images/giftools.gif') }}" alt="gif" />
-                    <h6 class="mb-0 f-w-400"><span class="font-primary">Don't Miss Out! </span><span class="f-light"> Out new update has been release.</span></h6>
-                    <i class="icon-arrow-top-right f-light"></i>
+                    <h6 class="mb-0 f-w-400">
+                        <span class="font-primary">Today's Date: </span>
+                        <span class="f-light">{{ \Carbon\Carbon::now()->format('F j, Y') }}</span>
+                    </h6>
+                    {{-- <i class="icon-arrow-top-right f-light"></i> --}}
                 </div>
             </div>
         </div>
