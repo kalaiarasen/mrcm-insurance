a25d0efdb7d2a826f06860c0c0269e77dc2f4266
Kalai <kalai@trisquare.com.my>
2025-12-19 11:06:52 +0800

    encryp password

--- resources/views/pages/policy-holder/index.blade.php
+++ resources/views/pages/policy-holder/index.blade.php
@@ -253,7 +253,7 @@
 
                         <div class="mb-3">
                             <label for="editCurrentPassword" class="form-label fw-bold">Current Password</label>
-                            <input type="text" class="form-control" id="editCurrentPassword" disabled
+                            <input type="text" class="form-control" id="editCurrentPassword" readonly
                                 name="current_password">
                             <div class="invalid-feedback d-block" id="currentPasswordError"></div>
                             <small class="text-muted">Leave blank if you don't want to change password</small>
